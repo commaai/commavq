27b42ed2ee9d2fd1ff7506275297a8df0b206fda
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2024-06-24 16:55:23 -0700

    don't assume cwd

--- compression/compress.py
+++ compression/compress.py
@@ -8,7 +8,9 @@ import numpy as np
 from pathlib import Path
 from datasets import load_dataset, DatasetDict
 
-output_dir = Path('./compression_challenge_submission/')
+HERE = Path(__file__).resolve().parent
+
+output_dir = HERE / './compression_challenge_submission/'
 
 def compress_tokens(tokens: np.ndarray) -> bytes:
   tokens = tokens.astype(np.int16).reshape(-1, 128).T.ravel().tobytes() # transposing increases compression rate ;)
@@ -27,15 +29,19 @@ def compress_example(example):
 if __name__ == '__main__':
   os.makedirs(output_dir, exist_ok=True)
   num_proc = multiprocessing.cpu_count()
+
   # load split 0 and 1
   splits = ['0', '1']
   ds = load_dataset('commaai/commavq', num_proc=num_proc, split=splits)
   ds = DatasetDict(zip(splits, ds))
+
   # compress
   ratios = ds.map(compress_example, desc="compress_example", num_proc=num_proc, load_from_cache_file=False)
+
   # make archive
-  shutil.copy('./compression/decompress.py', output_dir)
-  shutil.make_archive('compression_challenge_submission', 'zip', output_dir)
+  shutil.copy(HERE/'decompress.py', output_dir)
+  shutil.make_archive(HERE/'compression_challenge_submission', 'zip', output_dir)
+
   # print compression rate
-  rate = (sum(ds.num_rows.values()) * 1200 * 128 * 10 / 8) / os.path.getsize("compression_challenge_submission.zip")
+  rate = (sum(ds.num_rows.values()) * 1200 * 128 * 10 / 8) / os.path.getsize(HERE/"compression_challenge_submission.zip")
   print(f"Compression rate: {rate:.1f}")
