b95022c2174daab1cff0072dcdff1101cbd98936
Yassine <yassine.y10@gmail.com>
2024-06-24 21:17:41 -0700

    compression: only download split 0 and 1

--- compression/compress.py
+++ compression/compress.py
@@ -32,7 +32,8 @@ if __name__ == '__main__':
 
   # load split 0 and 1
   splits = ['0', '1']
-  ds = load_dataset('commaai/commavq', num_proc=num_proc, split=splits)
+  data_files = {'0': 'data_0_to_2500.zip', '1': 'data_2500_to_5000.zip'} # force huggingface datasets to only download these files
+  ds = load_dataset('commaai/commavq', num_proc=num_proc, split=splits, data_files=data_files)
   ds = DatasetDict(zip(splits, ds))
 
   # compress
